181bf9b8ce3add10f2d697c3819c73ff06c80387
Ostrega <Damian.Ostrega@unilever.com>
2018-07-06 15:53:57 +0200

    Add bunch of puts methods to the file

---
+++ The_puts_method.rb
@@ -0,0 +1,14 @@
+puts "Hello world"
+puts "I'm alive and well!"
+puts
+puts "I bought 5 dollars at the store"
+puts "I made $65 or a profit of 25%!"
+puts 5
+puts 3.14159
+puts 4+3
+puts "4"+"3"
+puts
+puts 3
+puts
+puts 
+puts "LajLajLaj"
