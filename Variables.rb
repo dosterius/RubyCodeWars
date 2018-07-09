f6eadf01a9249217707d9588305508c4c7b5d4be
Ostrega <Damian.Ostrega@unilever.com>
2018-07-09 16:50:23 +0200

    Add Variables.rb to repository

---
+++ Variables.rb
@@ -0,0 +1,24 @@
+name = "Damian"
+last_name = "Ostrega"
+handsome = true
+age = 30 + 2
+
+puts name
+puts last_name
+
+puts "Damian" + "Ostrega"
+puts name + last_name
+
+puts name + " " + last_name
+
+puts handsome
+
+puts age + 5 
+
+favorite_number = 24
+
+p favorite_number
+
+favorite_number = "Twenty-four"
+
+p favorite_number
