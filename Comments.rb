0656d798d9885ea835f83f4af2e8421c40c51a8b
Ostrega <Damian.Ostrega@unilever.com>
2018-07-09 12:38:34 +0200

    Add Comments.rb to repo

---
+++ Comments.rb
@@ -0,0 +1,25 @@
+# This is a comment
+
+# This prints out "Hello world" to the VSCode output
+puts "Hello world"
+
+# In this section of code,
+# we are going to do operations
+#   A, B, and C
+
+
+=begin
+  
+puts 1 + 1
+puts 5 + 3
+puts "Hello world"
+p "Blah bla bla"
+  
+=end 
+
+# ctrl + / comments selected text
+
+# puts 1 + 1
+# puts 5 + 3
+# puts "Hello world"
+# p "Blah bla bla"
