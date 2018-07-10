13f1f3097700759e810eefc3fe3d012ed1a30cb5
Ostrega <damian.ostrega@gmail.com>
2018-07-10 14:04:20 +0200

    Add Intro_to_Object_Methods file to repo

---
+++ Intro_to_Object_Methods.rb
@@ -0,0 +1,26 @@
+p "hello world".length
+
+p "hello world".upcase
+
+p "HELLO WORLD".downcase
+
+foo = "hello world"
+
+p foo.length
+p foo.upcase
+
+puts 
+
+p 10.next
+
+p 10.methods
+
+p -1.next
+
+puts
+
+puts "Hello world"
+puts "Hello world".inspect
+
+# p adds inspect method to puts
+p "Hello world"
