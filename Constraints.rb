5653934091add51d6843224ec9069922d111c8f6
Ostrega <damian.ostrega@gmail.com>
2018-07-10 10:12:10 +0200

    Add Constraints file to repo

---
+++ Constraints.rb
@@ -0,0 +1,18 @@
+age = 25
+p age
+
+age = 30
+p age
+
+NAME = "Damian"
+PI = 3.14159
+
+p NAME
+p PI
+
+p "My name is #{NAME}"
+
+#As we can see below constant can be reassign but it is vary bad habbit and has a big NONO from the community
+NAME = "Jeff"
+p NAME
+
