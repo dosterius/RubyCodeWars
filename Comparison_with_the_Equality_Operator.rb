bab18ac43a17f91a46f656cf7a180f231cdf0c07
Ostrega <damian.ostrega@gmail.com>
2018-07-19 15:45:01 +0200

    Add Comparison_with_the_Equality_Operatior.rb

---
+++ Comparison_with_the_Equality_Operator.rb
@@ -0,0 +1,22 @@
+p 10 == 10
+p 10 == 20
+
+puts
+a = 10
+b = 5
+c = 10
+
+p a == c
+p a == b
+p c == b
+ puts
+p "5" == 5 
+p 5 == 5
+
+p 5 == 5.0
+puts
+
+p 5.to_f.class
+p 5.0.class
+
+p 5 == 5.0.to_i
