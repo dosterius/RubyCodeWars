509195d7ed1b299ad7cf25c2c33240b32014dc93
Ostrega <damian.ostrega@gmail.com>
2018-07-19 15:54:58 +0200

    Add Comparison_with_the_Inequality_Operator.rb

---
+++ Comparison_with_the_Inequality_Operator.rb
@@ -0,0 +1,14 @@
+p 10 != 5
+p 10 != 10
+puts
+
+p "Hello" != "Goodbye"
+p "Hello" != "Hello"
+#case sensitive
+p "Hello" != "hello"
+
+p "Hello".downcase != "heLLo".downcase
+
+p "123" != 123
+
+p 123.0 != 123
