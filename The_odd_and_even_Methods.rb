2821853d35a68f957044a826fc877645bd5ecac7
Ostrega <damian.ostrega@gmail.com>
2018-07-19 14:25:37 +0200

    Add The_odd_and_even_Methods.rb

---
+++ The_odd_and_even_Methods.rb
@@ -0,0 +1,14 @@
+p 1.next
+p 1.to_s
+
+p 10.odd?
+p 10.even?
+
+p 11.odd?
+p 11.even?
+
+p 1234564569889234023.odd?
+p 1234564569889234024.odd?
+
+#float objects do not have this methods
+p 2.99.odd?
