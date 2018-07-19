d4cb2654357316c6f74e77c55db46be0519e04a7
Ostrega <damian.ostrega@gmail.com>
2018-07-19 14:09:02 +0200

    Add Convert_Numbers_to_Strings_and_Vice_Versa.rb

---
+++ Convert_Numbers_to_Strings_and_Vice_Versa.rb
@@ -0,0 +1,41 @@
+str = "5.8"
+
+p str
+p str.class
+
+puts
+
+str.to_i
+p str.to_i.class
+p str.class
+
+puts
+
+p str.to_f
+p str.class
+p str.to_f.class
+
+puts
+number = 10
+
+p number.class
+p number.to_s.class
+p number.to_f.class
+
+puts
+
+pi = 3.14
+
+p pi.to_i
+p pi.to_i.class
+
+puts
+
+p pi.to_s
+p pi.to_s.class
+
+puts
+#you can convert intiger to intiger ..string to string ..float to float.. it won't show error
+p "5".to_s.class
+p 10.to_i.class
+p 99.99.to_f.class
---
+++ Intro_to_Numbers.rb
@@ -0,0 +1,12 @@
+p 5.class
+p 0.class
+p 100.class
+p -837.class
+puts
+p 5.987.class
+p 2.71.class
+p -10.23.class
+puts
+p 99999999999999999999999999999999999999999999999999999999.class
+puts
+p 5.987.method
