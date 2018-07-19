70c7352768a5a9e27a55d261639c45efe1be9837
Ostrega <damian.ostrega@gmail.com>
2018-07-19 16:20:54 +0200

    Add Arithmetic_Methods.rb

---
+++ Arithmetic_Methods.rb
@@ -0,0 +1,38 @@
+
+#parenthesis are optional in case of methods
+p 1.next()
+
+puts
+
+#aritmethical figures like +,-,* etc are simply just methods used on a number
+# namened in the same way so we can use then both ways:
+p 1 + 2
+p 1.+(2)
+
+puts
+
+p 10 - 5
+p 10.-(5)
+p 10.- 5
+
+puts
+
+p 3 * 4
+p 3.*(4)
+
+puts
+
+p 10 / 5
+p 10./(5)
+p 10.div 5
+
+puts
+
+p 12.0 / 5.0
+p 12.0./(5.0)
+
+puts
+
+p 10 % 3
+p 10.%(3)
+p 10.modulo(3)
