cf1c8befc11cbdae2ac8592f12c5ff8b2c5f855c
Ostrega <damian.ostrega@gmail.com>
2018-08-07 12:25:20 +0200

    Add Assignements_and_Reassignments.rb

---
+++ Assignments_and_Reassignments.rb
@@ -0,0 +1,25 @@
+
+a = 10 
+p a
+
+#  a = a + 5
+# below is the same as above, it's the same with other operators
+a += 5 
+p a
+
+b = 100 
+
+#b = b - 50
+p b -= 50
+
+c = 3
+
+#c = c * 4
+c *= 4
+p c
+
+d = 40
+p d /= 4
+
+e = 43
+p e %= 3
