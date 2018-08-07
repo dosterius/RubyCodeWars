c1e4245da3182da84e4f551db5fa658f823b3ede
Ostrega <damian.ostrega@gmail.com>
2018-08-07 16:24:40 +0200

    Add Create_A_String.rb

---
+++ Create_A_String.rb
@@ -0,0 +1,31 @@
+
+puts "Hi there, I'm a string"
+puts "I can also include characters and numbers: # $ ! @ 5 9"
+
+name = "Damian"
+revenue = "$23 dollars"
+puts name, revenue
+
+space = " "
+
+p space.length
+
+puts
+
+empty = ""
+
+p empty.length
+
+puts
+
+p name.class
+p revenue.class
+p space.class
+p empty.class
+
+puts
+
+name = String.new("Damian)")
+p name
+
+p 5.to_s
