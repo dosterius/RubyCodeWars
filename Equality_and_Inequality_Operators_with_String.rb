58d11014f63277227aeaf49d0c0572f9b3795610
Ostrega <damian.ostrega@gmail.com>
2018-08-08 16:55:51 +0200

    Add Equality_and_Inequality_Operators_with_String.rb

---
+++ Equality_and_Inequality_Operators_with_String.rb
@@ -0,0 +1,26 @@
+
+a = "Hello"
+b = "hello"
+c = "Hello "
+
+p a != "Lion"
+
+p a == b
+p a == c
+p b == c
+
+puts
+
+p "Apple" < "Banana"
+p "hello" < "help"
+
+puts
+#capital lettres goes before lowercase
+p "A" < "a"
+p "Z" < "a"
+
+puts
+
+p true == true
+p true == false
+p false == false
