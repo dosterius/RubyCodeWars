39b5129b6bc7f8c966e1d5d2cb89107bb7145343
Ostrega <damian.ostrega@gmail.com>
2018-08-08 14:49:21 +0200

    Add Escape_Characters.rb

---
+++ Escape_Characters.rb
@@ -0,0 +1,17 @@
+
+puts "Hello world"
+puts 'Hello world'
+
+puts "Juliet said 'Goodbye' to Romeo"
+puts 'Juliet Said "Goodbye" to Romeo'
+
+#if we not gonna use slash below we will run into error
+puts "Juliet said \"Goodbye\" to Romeo"
+puts 'Juliet Said \'Goodbye\' to Romeo'
+
+result = "Let's add a line break\nright here"
+puts result
+p result
+
+puts "\tInsert a tab right here"
+puts "This is normal"
