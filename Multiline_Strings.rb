369bb03bb4fa820d1b199f694845442777afda6b
Ostrega <damian.ostrega@gmail.com>
2018-08-08 14:31:26 +0200

    Add Multiline_Strings.rb

---
+++ Multiline_Strings.rb
@@ -0,0 +1,21 @@
+
+words = <<MLS
+  This will be a multiline string
+when it is output.
+
+  The Tabs will also be visible.
+  Just you watch.
+  Bye!
+MLS
+
+p words
+
+puts 
+puts
+
+puts words
+
+puts
+puts
+
+print words
