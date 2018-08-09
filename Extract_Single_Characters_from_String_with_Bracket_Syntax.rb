f66839a3183d6ff17faa2f504b88657ea2bb86a1
Ostrega <damian.ostrega@gmail.com>
2018-08-09 11:49:07 +0200

    Add Extract_Single_Characters_from_String_with_Bracket_Syntax.rb

---
+++ Extract_Single_Characters_from_String_with_Bracket_Syntax.rb
@@ -0,0 +1,17 @@
+
+story = "Once upon a time in a land far, far away"
+
+p story.length
+
+puts
+
+p story[3]
+p story[-1]
+p story[100]
+
+puts
+
+# Square bracets and slice method are the same
+p story.slice(3)
+p story.slice(-1)
+p story.slice(100)
