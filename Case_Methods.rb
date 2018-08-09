d4de3e187817fbff5d19bc24f138b5ef8e08d755
Ostrega <damian.ostrega@gmail.com>
2018-08-09 16:59:42 +0200

    Add Case_Methods.rb

---
+++ Case_Methods.rb
@@ -0,0 +1,24 @@
+puts "hello".capitalize
+puts "heLLo".capitalize
+puts "Hello123#$#@".capitalize
+
+puts
+
+#it will always capitalize the first character in the string
+puts "hello world".capitalize
+
+puts
+
+puts "damian123".upcase
+puts "lajlajlaj".upcase
+
+lowercase = "I'm patient"
+uppercase = lowercase.upcase
+p uppercase
+
+#capitalize, upcase and downcase will not affect the numbers and symbols
+
+p "DAMIAN@#%$#234".downcase
+
+#swapcase revert string 
+p "daMIan".swapcase
