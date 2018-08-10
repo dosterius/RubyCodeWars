be9cc68192e5afc7cc4e69f923ab7eaade59cef8
Ostrega <damian.ostrega@gmail.com>
2018-08-10 11:59:56 +0200

    Bang_Methods_on_Strings.rb

---
+++ Bang_Methods_on_Strings.rb
@@ -0,0 +1,21 @@
+word = "hello"
+
+#bang makse change on string permament
+p word.capitalize!
+p word
+
+word.upcase!
+
+p word
+
+word.downcase
+
+p word
+
+word.reverse!
+
+p word
+
+word.swapcase!
+
+p word
