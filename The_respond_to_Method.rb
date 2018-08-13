2a875347d60bbfcd6e622ab6b5935f39cb8c64f1
Ostrega <damian.ostrega@gmail.com>
2018-08-13 15:19:06 +0200

    Add The_respond_to_Method.rb

---
+++ The_respond_to_Method.rb
@@ -0,0 +1,16 @@
+num = 1000
+
+if num.respond_to?("next")
+  p num.next
+end
+
+#p num.respond_to?("even?")
+
+#p num.respond_to?("length")
+
+puts "Hello".respond_to?("length")
+puts "Hello".respond_to?("odd?")
+puts "Hello".respond_to?("next")
+
+# you can use symbols object to do the same with string object. It's more lightweight
+puts "Hello".respond_to?(:length)
