08fb04da342f2400327358293244774f95ad0a4b
Ostrega <damian.ostrega@gmail.com>
2018-08-13 15:24:42 +0200

    Add custom method to see if repond_to? works on it

--- The_respond_to_Method.rb
+++ The_respond_to_Method.rb
@@ -8,9 +8,20 @@ end
 
 #p num.respond_to?("length")
 
+def show(laj)
+  laj
+end
+
 puts "Hello".respond_to?("length")
 puts "Hello".respond_to?("odd?")
 puts "Hello".respond_to?("next")
 
 # you can use symbols object to do the same with string object. It's more lightweight
 puts "Hello".respond_to?(:length)
+
+puts
+
+#why is it not reponding to custom made method?
+puts "Hello".respond_to?(:show)
+
+p show("Hello")
