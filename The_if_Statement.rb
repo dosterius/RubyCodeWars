aef7470dacb99b0695b08437a23ff8d24602ccb4
Ostrega <damian.ostrega@gmail.com>
2018-08-13 09:52:30 +0200

    Add The_if_Statement.rb

---
+++ The_if_Statement.rb
@@ -0,0 +1,29 @@
+if 5 < 7
+  puts "That math statement is true!"
+end
+
+if 5 < 2
+  puts "That math statement is true!"
+end
+
+password = "topsecret"
+
+if password == "topsecret"
+  puts "Congrats, you've logged into our system!"
+end
+
+word = "kangaroo"
+
+if word.length == 8
+  puts "That word has 8 letters!"
+end
+
+word = "zebra"
+
+if word.include?("eb")
+  puts "Yup, your word has eb in it!"
+end
+
+if 5.odd?
+  puts "That number is odd"
+end
