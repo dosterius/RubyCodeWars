126959304c35e8dba03ca606da2f72d7eb331879
Ostrega <damian.ostrega@gmail.com>
2018-08-13 10:02:52 +0200

    Add Truthiness_and_Falsiness.rb

---
+++ Truthiness_and_Falsiness.rb
@@ -0,0 +1,26 @@
+if true
+  puts "That is true"
+end
+
+if false
+  puts "That is true"
+end
+
+if nil
+  puts "That is true"
+end
+
+# truthee value 5 is true or otherwords it's entough true for ruby to evaluate it's true
+# there are 2 falsee values: 'false' and 'nil' everything else will return true in ruby and compile the code inside if statement
+
+if 5
+  puts "That is true"
+end
+
+if "This is true"
+  puts "That is true"
+end
+
+if {}
+  puts "That is true"
+end
