fdfc133e181168b32e5381367171607565a64c30
Ostrega <damian.ostrega@gmail.com>
2018-08-14 16:30:04 +0200

    Add Negation_with_bang.rb

---
+++ Negation_with_bang.rb
@@ -0,0 +1,21 @@
+user = "free"
+
+if user != "subscriber"
+  puts "Only subscribers are allowded!"
+end
+
+puts !true
+puts !false
+
+puts !1
+
+puts
+
+puts !nil
+
+# double bang gives you boolean values with negating the negation (you can do it as log as you want)
+puts !!""
+
+puts !!1
+puts
+puts !!!1
