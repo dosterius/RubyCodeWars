5dcff4dc997f41ef1b65dcc8378f756859beaa9a
Ostrega <damian.ostrega@gmail.com>
2018-08-20 17:15:34 +0200

    Add The_while_Keyword.rb

---
+++ The_while_Keyword.rb
@@ -0,0 +1,37 @@
+# i = 1 
+
+# while i <= 10
+#   puts i
+#   i += 1
+# end
+
+# puts
+
+# p i
+
+status = true
+
+while status
+  print "Please enter username: "
+  username = gets.chomp.downcase
+  print "Please enter password: "
+  password = gets.chomp.downcase
+
+
+  if username == "damian" && password == "bestpasswordever"
+    puts "Entry granted. The nuclear codes are..."
+    status = false
+  elsif username == "quit" || password == "quit"
+    puts "Goodbye! Better luck next time!"
+    status = false
+  else 
+    puts "Incorrect combination, try again or enter 'quit'"
+  end
+end
+
+i = 2
+
+while i.even? && i < 100
+  puts i
+  i += 2
+end
