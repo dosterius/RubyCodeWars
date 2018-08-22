70e330dfe2b0dca7f719a6a2beba7952289ce3cb
Ostrega <damian.ostrega@gmail.com>
2018-08-22 15:57:00 +0200

    Add Intoduciton_to_Arrays.rb

---
+++ Introduction_to_Arrays.rb
@@ -0,0 +1,28 @@
+p []
+p [].class 
+
+numbers = [4, 8, 15, 16, 23, 42]
+
+puts numbers
+p numbers
+print numbers
+
+toys  = ["Teddy bear", "Super Soaker", "Board Game"]
+
+puts toys
+p toys
+print toys
+
+things = [4, true, "Hello", 10.99]
+
+p things
+
+registrations = [true, true, false, true, false]
+p registrations 
+
+#multidimentional array
+students = [["Damian", 32, true], 
+            ["Sally", 23, true],
+            ["Stephan", 40, false]]
+
+puts students
