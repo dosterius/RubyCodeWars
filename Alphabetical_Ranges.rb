e4f6f5d606c9f34a2d50d66a73796f8b314b2700
Ostrega <damian.ostrega@gmail.com>
2018-08-22 12:46:45 +0200

    Rename file Alphabetical_Rangers.rb part II

---
+++ Alphabetical_Ranges.rb
@@ -0,0 +1,28 @@
+
+puts (1..10).first(3)
+
+alphabet = "a".."z"
+puts alphabet.first(5)
+
+puts 
+
+puts alphabet.last(1)
+
+puts
+
+alphabet = "A".."z"
+puts alphabet.first(40)
+
+puts 
+
+puts alphabet.last(1)
+
+puts
+
+alphabet = "A".."Z"
+puts alphabet.first(40)
+
+puts 
+
+p alphabet.first(40)
+puts alphabet.last(1)
