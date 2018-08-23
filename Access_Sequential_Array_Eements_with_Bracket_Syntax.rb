8ac392d84cc40b1d810f164090b38334a29199c1
Ostrega <damian.ostrega@gmail.com>
2018-08-23 13:41:49 +0200

    Add Access_Sequential_Array_Elements_with_a_Rabge_Object.rb

---
+++ Access_Sequential_Array_Eements_with_Bracket_Syntax.rb
@@ -0,0 +1,13 @@
+
+numbers = [1, 3, 5, 7, 9, 15, 21]
+
+# second numeric argument is number of items that you want to extract from an array
+p numbers[2, 4]
+
+p numbers[1, 3]
+
+#one result is array class the second is fixnum
+p numbers[0, 1]
+p numbers[0]
+
+p numbers[1,100]
