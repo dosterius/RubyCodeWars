b67d90daaa21bc17ba260217b85e50239276547f
Ostrega <damian.ostrega@gmail.com>
2018-08-23 15:45:05 +0200

    Add Overwrite_ine_or_More_Array_Elements.rb

---
+++ Overwrite_one_or_More_Array_Elements.rb
@@ -0,0 +1,27 @@
+
+fruits = ["Apple", "Orange", "Grape", "Bannana"]
+p fruits
+
+fruits[1] = "Watermelon"
+p fruits
+
+fruits[-1] = "Bananas"
+p fruits
+
+fruits[4] = "Raspberry"
+p fruits
+
+fruits[5] = "Strawberry"
+p fruits
+
+fruits[10] = "Kiwi"
+p fruits
+
+fruits[3, 2] = ["Canteloupe","Dragonfruit"] 
+p fruits
+
+fruits[0..2] = ["Blackberry", "Orange", "Pears"]
+p fruits
+
+fruits[0..3] = ["blah"]
+p fruits
